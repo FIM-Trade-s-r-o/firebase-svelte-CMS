22cb3eaec3de2dbbd9233cec4925d3b10d2a02ed
hejtmus <filip.holcik.official@gmail.com>
2022-02-03 23:58:46 +0100

    Prepared dark theme

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import 'bootstrap/scss/bootstrap.scss';
+    import '$lib/scss/custom.scss'
     import 'bootstrap-icons/font/bootstrap-icons.css';
     import '$lib/locales/index.ts';
 </script>
