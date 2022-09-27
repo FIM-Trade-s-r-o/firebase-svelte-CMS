47a312a572d5ed2682b91807abc45ecbd76d3343
Filip Holčík <filip.holcik.official@gmail.com>
2022-09-27 20:57:02 +0200

    Outlined JWT auth

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,14 +6,15 @@ import {
 } from '@firebase/auth'
 import { auth } from '$lib/firebase'
 
-const login = async ({ request }) => {
+const login = async ({ request, cookies }) => {
     const data = await request.formData()
     const email = data.get('email')
     const password = data.get('password')
 
     const adminAccount = await config.getAdminAccount(email)
     if (adminAccount) {
-        const token = config.login(adminAccount, email, password)
+        const token = config.login(adminAccount, password)
+        cookies.set('sessionId', token)
         return {
             name: email,
             token
