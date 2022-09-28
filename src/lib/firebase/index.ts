700764d165de900df5b7b1badb4a99c4e3b596dc
Filip Holčík <filip.holcik.official@gmail.com>
2022-09-28 19:45:58 +0200

    Prepared firebase storage rework

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -4,11 +4,12 @@ import { getStorage } from 'firebase-admin/storage'
 import firebaseConfig from './config.json'
 
 const app = initializeApp({
-    credential: cert(firebaseConfig)
+    credential: cert(firebaseConfig.credential),
+    ...firebaseConfig.rest
 })
 
 const firestore = getFirestore(app)
-const storage = getStorage(app)
+const storage = getStorage(app).bucket()
 
 export {
     firestore,
--- src/routes/dashboard/storage/[folder]/+page.server.ts
+++ src/routes/dashboard/storage/[folder]/+page.server.ts
@@ -1,4 +1,4 @@
-import { uploadString, uploadBytes, deleteObject, ref, listAll, getDownloadURL } from 'firebase/storage'
+// import { uploadString, uploadBytes, deleteObject, ref, listAll, getDownloadURL } from 'firebase/storage'
 import { storage } from '$lib/firebase'
 
 const pathToSlashPath = (path: string): string => {
@@ -13,27 +13,27 @@ const newFolder = async ({ request }) => {
     const path = pathToSlashPath(data.get('path'))
     const folderName = data.get('folderName')
     if (folderName) {
-        const directory = ref(storage, path)
-        const newDirectory = ref(directory, folderName)
-        const ghostFile = ref(newDirectory, '.ghostfile')
-        await uploadString(ghostFile, '')
+        // const directory = ref(storage, path)
+        // const newDirectory = ref(directory, folderName)
+        // const ghostFile = ref(newDirectory, '.ghostfile')
+        // await uploadString(ghostFile, '')
     }
 }
 const uploadFile = async (reference, file) => {
-    const uploadRef = ref(reference, file.name)
-    await uploadBytes(uploadRef, await file.arrayBuffer())
+    // const uploadRef = ref(reference, file.name)
+    // await uploadBytes(uploadRef, await file.arrayBuffer())
 }
 const newFile = async ({ request }) => {
     const data = await request.formData()
     const path = pathToSlashPath(data.get('path'))
     const files = data.getAll('files[]')
-    const directory = ref(storage, path)
+    // const directory = ref(storage, path)
     if (files.length > 0) {
         let uploads = []
         for (const file of files) {
             uploads = [
                 ...uploads,
-                uploadFile(directory, file)
+                // uploadFile(directory, file)
             ]
         }
         await Promise.all(uploads)
@@ -49,27 +49,29 @@ const newFile = async ({ request }) => {
 const deleteFolder = async ({ request }) => {
     const data = await request.formData()
     const path = pathToSlashPath(data.get('path'))
-    const ghostFileRef = ref(storage, `${path}/.ghostfile`)
-    await deleteObject(ghostFileRef)
+    // const ghostFileRef = ref(storage, `${path}/.ghostfile`)
+    // await deleteObject(ghostFileRef)
 }
 const deleteFile = async ({ request }) => {
     const data = await request.formData()
     const path = pathToSlashPath(data.get('path'))
     const name = data.get('name')
-    const ghostFileRef = ref(storage, `${path}/${name}`)
-    await deleteObject(ghostFileRef)
+    // const ghostFileRef = ref(storage, `${path}/${name}`)
+    // await deleteObject(ghostFileRef)
 }
 
 export async function load ({ params }) {
     const path = pathToSlashPath(params.folder)
-    const reference = ref(storage, path.substring(1))
+    // const reference = ref(storage, path.substring(1))
 
-    const response = await listAll(reference)
+    // const response = await listAll(reference)
+    const response = await storage.getFiles({ prefix: path.substring(1), delimiter: '/', autoPaginate: false })
 
+    console.log(response)
     let folders = []
     let filePromises = []
 
-    response.prefixes.forEach((folderRef) => {
+    /*response.prefixes.forEach((folderRef) => {
         folders = [
             ...folders,
             {
@@ -90,7 +92,7 @@ export async function load ({ params }) {
                 })()
             ]
         }
-    })
+    })*/
 
     return {
         path: pathToPipePath(path),
